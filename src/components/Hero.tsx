d5c25fdc86c3d8c4ea4efe6b224aea945de560da
eamarian <eamarian@gmail.com>
2024-11-18 15:17:48 -0600

    Hero redesign -> switch from typeit to text rotation animation.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,25 +1,19 @@
-import Image from "next/image";
-import TypeIt from "typeit-react";
-import { default as TypeItCore } from "typeit";
-import { forwardRef, MouseEventHandler, useState, CSSProperties } from "react";
+import { forwardRef, MouseEventHandler } from "react";
 
 interface HeroProps {
   setCanScroll: (canScroll: boolean) => void;
 }
 
-const titles: { title: string; color: CSSProperties["color"] }[] = [
-  { title: "Nature Lover", color: "green" },
-  { title: "Caffeine Addict", color: "violet" },
-  { title: "Book Worm", color: "brown" },
-  { title: "Dog Dad", color: "orange" },
-  { title: "Game Freak", color: "red" }, //I don't know if I like this one...
-  { title: "Software Developer", color: "violet" },
+const titles: { title: string; colorClassName: string }[] = [
+  { title: "Nature Lover", colorClassName: "text-green-500" },
+  { title: "Caffeine Addict", colorClassName: "text-violet-500" },
+  { title: "Book Worm", colorClassName: "text-amber-500" },
+  { title: "Dog Dad", colorClassName: "text-orange-500" },
+  { title: "Game Freak", colorClassName: "text-red-500" }, //I don't know if I like this one...
+  { title: "Software Developer", colorClassName: "text-blue-500" },
 ];
 
 const Hero = forwardRef<HTMLElement, HeroProps>((props, ref) => {
-  const [animationIsComplete, setAnimationIsComplete] =
-    useState<boolean>(false);
-
   const onClick: MouseEventHandler<HTMLAnchorElement> = (
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent> // eslint-disable-line @typescript-eslint/no-unused-vars
   ) => {
@@ -28,52 +22,36 @@ const Hero = forwardRef<HTMLElement, HeroProps>((props, ref) => {
   return (
     <section
       id="home"
-      className="flex flex-auto flex-col items-center text-center sp gap-5 h-screen"
+      className="flex flex-auto flex-col items-center sp gap-0 h-screen"
       ref={ref}
     >
-      <div></div>
-      <Image
-        src="/profile.svg"
-        alt="Profile picture"
-        priority={true}
-        width={180}
-        height={180}
-      ></Image>
-      <TypeIt
-        as="p"
-        options={{
-          //lifeLike: true,
-          speed: 100,
-          afterComplete: (instance: TypeItCore) => {
-            instance.destroy();
-            setAnimationIsComplete(true);
-          },
-        }}
-        getBeforeInit={(instance) => {
-          instance
-            .type("Hi, ")
-            .type("I'm <em style='color:red'>Evan Marian</em>.")
-            .pause(500)
-            .break()
-            .type("I'm a ");
-          titles.map((value, index) => {
-            instance
-              .type(`<em style='color:${value.color}'>${value.title}</em>`)
-              .pause(200);
-
-            if (index < titles.length - 1) instance.delete(value.title.length);
-          });
-          return instance;
-        }}
-      ></TypeIt>
+      <div className="outer-headings">
+        <h1>
+          Hi, <br />
+          I&apos;m Evan Marian.
+          <br />
+          <div className="line">
+            <div className="static-text">{"I'm a "}</div>
+            <div className="rotate-text">
+              <span>
+                {titles.map(({ title, colorClassName }, index) => {
+                  return (
+                    <span key={index} className={colorClassName}>
+                      {title}.
+                      <br />
+                    </span>
+                  );
+                })}
+                <span key={0} className={titles[0].colorClassName}>
+                  {titles[0].title}.<br />
+                </span>
+              </span>
+            </div>
+          </div>
+        </h1>
+      </div>
       {
-        <a
-          className={`transition-opacity duration-1000 ${
-            animationIsComplete ? "opacity-100" : "opacity-0"
-          }`}
-          href={animationIsComplete ? "#about" : ""}
-          onClick={onClick}
-        >
+        <a className="animate-bounce" href={"#about"} onClick={onClick}>
           Find Out More
         </a>
       }
